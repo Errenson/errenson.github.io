a0f28f35f3b92657d9d3977ab3f7d29bce6e1e8b
Erren <e.errenson@gmail.com>
2014-11-13 22:21:01 +0800

    v0.9

--- j/route.js
+++ j/route.js
@@ -6,7 +6,6 @@
 
   function* index(c) { // c:context
     key.setScope('write')
-    console.log(c.path);
     if (!c.state.ul1) {
       c.state.ul1 =
         yield $agj('https:/api.github.com/repos/Errenson/static-blog-posts/contents/时间管理')
@@ -16,17 +15,18 @@
         yield $agj('https:/api.github.com/repos/Errenson/static-blog-posts/contents/Programming')
       c.state.article =
         yield $agr('https:/api.github.com/repos/Errenson/static-blog-posts/readme')
+        //show leftbar
+      addLeftbar(c.state.ul1, 1)
+      addLeftbar(c.state.ul2, 2)
+      addLeftbar(c.state.ul3, 3)
+        //enable keyboard navigation
+      $each($$('leftbar li'), function(li, index) {
+        li.setAttribute('keyCue', Setting.lbKey[index]);
+      })
     }
-    //show leftbar
-    addLeftbar(c.state.ul1, 1)
-    addLeftbar(c.state.ul2, 2)
-    addLeftbar(c.state.ul3, 3)
-      //show readme.md
+    //show readme.md
     $('article').innerHTML = marked(c.state.article)
-      //enable keyboard navigation
-    $each($$('leftbar li'), function(li, index) {
-      li.setAttribute('keyCue', Setting.lbKey[index]);
-    })
+    document.body.scrollTop=0
   }
 
   function addLeftbar(arr, num) {
@@ -36,7 +36,7 @@
   }
 
   function* showArticle(c) {
-    console.log(c.path);
+    document.body.scrollTop=0
     document.title = c.path.substr(1)
     if (!c.state.a) {
       c.state.a =
@@ -50,9 +50,6 @@
   for (path in routes) {
     page(path, auco(routes[path]))
   }
-  page('*', function() {
-    $('body').innerText = 'Not found!!'
-  })
   page()
     // auto coroutine //////////////////////////////////
   function auco(g) {
