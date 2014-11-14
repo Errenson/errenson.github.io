0657efe34f315e0ea60675ea111260f0fbe72d96
Erren <e.errenson@gmail.com>
2014-11-14 15:20:42 +0800

    no message

--- j/route.js
+++ j/route.js
@@ -5,18 +5,20 @@
   }
 
   function* index(c) { // c:context
+    //reset
     key.setScope('write')
     document.title = 'Erren\'s static blog'
-    if (!c.state.ul1) {
+    document.body.scrollTop = 0
+    var liFocus = $('leftbar li.focus')
+    if (liFocus) $cr(liFocus, 'focus')
+    //show leftbar
+    if (!$('leftbar li')) {
       c.state.ul1 =
         yield $agj('https:/api.github.com/repos/Errenson/static-blog-posts/contents/时间管理')
       c.state.ul2 =
         yield $agj('https:/api.github.com/repos/Errenson/static-blog-posts/contents/环境搭建')
       c.state.ul3 =
         yield $agj('https:/api.github.com/repos/Errenson/static-blog-posts/contents/Programming')
-      c.state.article =
-        yield $agr('https:/api.github.com/repos/Errenson/static-blog-posts/readme')
-        //show leftbar
       addLeftbar(c.state.ul1, 1)
       addLeftbar(c.state.ul2, 2)
       addLeftbar(c.state.ul3, 3)
@@ -27,13 +29,12 @@
       c.save()
     }
     //show readme.md
+    if(!c.state.article){ 
+      c.state.article =
+        yield $agr('https:/api.github.com/repos/Errenson/static-blog-posts/readme')
+    }
     $('article').innerHTML = marked(c.state.article)
-    //reset
-    document.body.scrollTop = 0
-    var liFocus = $('leftbar li.focus')
-    if (liFocus) $cr(liFocus, 'focus')
   }
-
   function addLeftbar(arr, num) {
     $each(arr, function(obj) {
       $('#ul-' + num).innerHTML += $elc(obj.name.slice(0, -3))
