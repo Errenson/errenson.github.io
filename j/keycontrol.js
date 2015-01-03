65995bfa3d60c6e863c753339d2137a57c3bf4be
Erren <e.errenson@gmail.com>
2015-01-03 16:02:23 +0800

    fixed

--- j/keycontrol.js
+++ j/keycontrol.js
@@ -10,7 +10,7 @@ function leftbarKeyGen(a) {
 		if(liFocus) $cr(liFocus,'focus')
 		$ca(li,'focus')
 
-		var ulName=['','时间管理','环境搭建','Programming']
+		var ulName=['','人生管理','工具','Programming']
 		State.gpath=ulName[li.parentElement.id.substr(-1)]+'/'+li.innerText
 		page('/' + li.innerText)
 	}
